c817e431138cc8954ec99dfb6cf27bf6b1022d6d
O-L-ope <100943896+O-L-ope@users.noreply.github.com>
2022-05-13 13:22:31 +0200

    Add files via upload

---
+++ Cull.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+    /*
+     * klasa opisująca konkretny rodzaj itemu w sklepie
+     */
+    public class Cull : Item
+    {
+        //deprecated???
+        //public int bonusHP = 150;
+        public Cull()
+        {
+            //ustalamy nazwe itemu
+            name = "Cull";
+            // jego cene
+            price = 450;
+            //i atrybuty
+            AddStat(Stat.HP, 0);
+            AddStat(Stat.HOH, 3);
+            AddStat(Stat.AD, 7);
+        }
+    }
+}
